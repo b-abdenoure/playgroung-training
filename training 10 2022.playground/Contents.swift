31ba339674359cc52d912618e47955346c293ade
b-abdenoure <95860782+b-abdenoure@users.noreply.github.com>
2022-10-30 23:00:21 +0100

    Add files via upload

---
+++ training 10 2022.playground/Contents.swift
@@ -0,0 +1,25 @@
+//
+//var a: Float = 5
+//var b: Float = 2
+//
+//print(Float(a / b))
+////////////////////////
+let array2d = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+]
+print(array2d [2][2])
+////////////////////////
+let array2 = [3, 2, 4, 7]
+var number = 3
+//var track = 4
+if number + 1 == array2.count{
+//if number + 1 == array2 [2]{
+//    number /*+= track*/
+    print("match")
+    print(array2.count)
+}else{
+    print("not matching")
+}
+print(number)
